7f7dbb16df83bc7d96e6a4af1387e5a8f5af9cac
RijalAzmi <76202930+RijalAzmi@users.noreply.github.com>
2021-04-05 18:16:09 +0700

    file listing

---
+++ Listing 3.1 - 3.9/BigIntegerTest.java
@@ -0,0 +1,25 @@
+package com.tutorial;
+
+import java.util.*;
+import java.math.*;
+
+public class BigIntegerTest {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.print("How many numbers do you need to draw? ");
+		int k = in.nextInt();
+		
+		System.out.print("What it teh highest number you can draw? ");
+		int n = in.nextInt();
+		
+		BigInteger lotteryOdds = BigInteger.valueOf(1);
+		
+		for (int i =1; i <= k; i++)
+			lotteryOdds = lotteryOdds.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
+		
+		System.out.print("Your odds are 1 in " + lotteryOdds + ". Good luck");
+
+	}
+
+}
---
+++ Listing 3.1 - 3.9/CompoundInterest.java
@@ -0,0 +1,10 @@
+package com.tutorial;
+
+public class CompoundInterest {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}

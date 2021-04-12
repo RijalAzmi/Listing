c019e0b0e2d2e8d038de824ed0fb01a1b002ceff
RijalAzmi <76202930+RijalAzmi@users.noreply.github.com>
2021-04-12 14:53:54 +0700

    listing 4.4

--- Listing 4.1 - 4.4/Listing 4.4/src/Listing/ParamTest.java
+++ Listing 4.1 - 4.4/Listing 4.4/src/Listing/ParamTest.java
@@ -1,6 +1,7 @@
+package Listing;
+
 public class ParamTest {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		/*
 		 * Test 1: Methods can't modify numeric parameters
 		 */
@@ -9,7 +10,7 @@ public class ParamTest {
 		System.out.println("Before: percent=" + percent);
 		tripleValue(percent);
 		System.out.println("After: percent=" + percent);
-	
+
 		/*
 		 * Test 2: Methods can change the state of object parameters
 		 */
@@ -18,7 +19,7 @@ public class ParamTest {
 		System.out.println("Before: salary=" + harry.getSalary());
 		tripleSalary(harry);
 		System.out.println("After: salary=" + harry.getSalary());
-	
+
 		/*
 		 * Test 3: Methods can't attach new objects to object parameters
 		 */
@@ -31,52 +32,47 @@ public class ParamTest {
 		System.out.println("After: a=" + a.getName());
 		System.out.println("After: b=" + b.getName());
 	}
-	
-public static void tripleValue(double x) // doesn't work
-{
-	x = 3 * x;
-	System.out.println("End of method: x=" + x);
-}
 
-public static void tripleSalary(Employee x) // works
-{
-	x.raiseSalary(200);
-	System.out.println("End of method: salary=" + x.getSalary());
-}
-	
-public static void swap(Employee x, Employee y)
-{	
-	Employee temp = x;
-	x = y;
-	y = temp;
-	System.out.println("End of method: x=" + x.getName());
-	System.out.println("End of method: y=" + y.getName());
+	public static void tripleValue(double x) // doesn't work
+	{
+		x = 3 * x;
+		System.out.println("End of method: x=" + x);
+	}
+
+	public static void tripleSalary(Employee x) // works
+	{
+		x.raiseSalary(200);
+		System.out.println("End of method: salary=" + x.getSalary());
+	}
+
+	public static void swap(Employee x, Employee y) {
+		Employee temp = x;
+		x = y;
+		y = temp;
+		System.out.println("End of method: x=" + x.getName());
+		System.out.println("End of method: y=" + y.getName());
 	}
 }
-	
+
 class Employee // simplified Employee class
 {
 	private String name;
 	private double salary;
-	
-	public Employee(String n, double s)
-	{
+
+	public Employee(String n, double s) {
 		name = n;
 		salary = s;
 	}
-	
-	public String getName()
-	{
+
+	public String getName() {
 		return name;
 	}
-	
-	public double getSalary()
-	{
+
+	public double getSalary() {
 		return salary;
 	}
-	
-	public void raiseSalary(double byPercent)
-	{
+
+	public void raiseSalary(double byPercent) {
 		double raise = salary * byPercent / 100;
 		salary += raise;
 	}
